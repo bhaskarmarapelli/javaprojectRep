3dbbf991fc519bb5eab7006bb1e02cabfa74c94b
wku <wku@DESKTOP-UBNH049>
2019-11-05 09:21:06 +0300

    main method is added and verified

---
+++ mygitproject/src/mygitpackage/MainClass.java
@@ -0,0 +1,17 @@
+package mygitpackage;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student obj=new Student();
+		
+		obj.setName("bhaskar");
+		obj.setId(1234);
+		System.out.println("name:"+obj.getName()+" id:"+obj.getId());
+		
+		
+	}
+
+}
