b806b7dfdc92232ddffaaf04ca8cf3d978ec6eb2
wku <wku@DESKTOP-UBNH049>
2019-11-05 09:39:05 +0300

    person class adeded

---
+++ mygitproject/src/mygitpackage/Person.java
@@ -0,0 +1,15 @@
+package mygitpackage;
+
+public class Person {
+
+	String name;
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+}
