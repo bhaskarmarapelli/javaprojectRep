baa5b03083f0c85a51aa05a2391c40bdd564dcc4
bhasksar <bhaskar@gmail.com>
2019-11-05 09:16:12 +0300

    added student class get and set methods

---
+++ mygitproject/src/mygitpackage/Student.java
@@ -0,0 +1,23 @@
+package mygitpackage;
+
+public class Student {
+
+	int id;
+	String name;
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+	public int getId()
+	{
+		return this.id;
+	}
+}
